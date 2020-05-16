cc6eb47bca0c1a849a3b061d68c260e645015dad
Alex Moses <alexander_moses@hotmail.com>
2020-05-16 22:03:01 +1000

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,12 +3,12 @@ import { Link } from 'gatsby'
 import styles from './navigation.module.css'
 
 export default () => (
-  <nav role="navigation">
+<! ––  <nav role="navigation">
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
         <Link to="/">Home</Link>
       </li>
      
     </ul>
-  </nav>
+  </nav> ––> 
 )
