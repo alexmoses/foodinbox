481a6d37f83f11bd1fb9c94ca840d1fd6e5bfadb
Alex Moses <alexander_moses@hotmail.com>
2020-07-11 15:03:27 +1000

    Get rid of ugly Hero Header

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ class RootIndex extends React.Component {
       <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
           <Helmet title={siteTitle} />
-          <Hero data={author.node} />
+          {/*<Hero data={author.node} />*/}
           <div className="wrapper">
             <h2 className="section-headline">Recent articles</h2>
             <ul className="article-list">
