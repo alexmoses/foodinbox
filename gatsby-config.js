79ee9a516147f496eba0bebab3d41a884a2832fa
Alex Moses <alexander_moses@hotmail.com>
2020-07-12 11:37:16 +1000

    ANalytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,14 @@ module.exports = {
   },
   pathPrefix: '/gatsby-contentful-starter',
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-172429071-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true
+      }
     'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
