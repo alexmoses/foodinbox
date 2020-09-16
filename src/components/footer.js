4c92c7b4ea442776600a72c81c04da255d1d7569
Alex Moses <alexander_moses@hotmail.com>
2020-09-17 09:48:51 +1000

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,16 @@
 import React from "react";
 import styles from './footer.css'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook, faInstagram, faTwitter, faPinterest  } from '@fortawesome/free-solid-svg-icons'
 
 
 const Footer = () => (
   <div className="footer">
-   <Link to="https://www.instagram.com/thefoodinbox/"><FontAwesomeIcon icon={instagram} /></Link>
-   <Link to="https://www.facebook.com/thefoodinbox"><FontAwesomeIcon icon={facebook} /></Link>
-   <Link to="https://twitter.com/OurFoodInbox"><FontAwesomeIcon icon={twitter} /></Link>
-   <Link to="https://www.pinterest.com.au/thefoodinbox/"><FontAwesomeIcon icon={pinterest} /></Link>
+   <Link to="https://www.instagram.com/thefoodinbox/"><FontAwesomeIcon icon={["fab", "instagram"]} /></Link>
+   <Link to="https://www.facebook.com/thefoodinbox"><FontAwesomeIcon icon={["fab", "facebook"]} /></Link>
+   <Link to="https://twitter.com/OurFoodInbox"><FontAwesomeIcon icon={["fab", "twitter"]} /></Link>
+   <Link to="https://www.pinterest.com.au/thefoodinbox/"><FontAwesomeIcon icon={["fab", "pinterest"]} /></Link>
   </div>
 );
 
