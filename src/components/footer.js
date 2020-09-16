12bd69edf460cf6bd1556969edc5f44986027be3
Alex Moses <alexander_moses@hotmail.com>
2020-09-17 09:52:53 +1000

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faFacebook, faInstagram, faTwitter, faPinterest  } from '@fortawesome/free-solid-svg-icons'
 
+library.add(fab, faFacebook, faInstagram, faTwitter, faPinterest)
+
 
 const Footer = () => (
   <div className="footer">
