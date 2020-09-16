00dd6e4f710df15893a19fb50b2c6a24b76f40c3
Alex Moses <alexander_moses@hotmail.com>
2020-09-17 09:34:36 +1000

    Footer with updated icon codes

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,10 @@ import React from "react";
 import styles from './footer.css'
 const Footer = () => (
   <div className="footer">
-   <Link to="https://www.instagram.com/thefoodinbox/"><FontAwesomeIcon icon="fa-instagram" /></Link>
-   <Link to="https://www.facebook.com/thefoodinbox"><FontAwesomeIcon icon="fa-facebook" /></Link>
-   <Link to="https://twitter.com/OurFoodInbox"><FontAwesomeIcon icon="fa-twitter" /></Link>
-   <Link to="https://www.pinterest.com.au/thefoodinbox/"><FontAwesomeIcon icon="fa-pinterest" /></Link>
+   <Link to="https://www.instagram.com/thefoodinbox/"><FontAwesomeIcon icon={instagram} /></Link>
+   <Link to="https://www.facebook.com/thefoodinbox"><FontAwesomeIcon icon={facebook} /></Link>
+   <Link to="https://twitter.com/OurFoodInbox"><FontAwesomeIcon icon={twitter} /></Link>
+   <Link to="https://www.pinterest.com.au/thefoodinbox/"><FontAwesomeIcon icon={pinterest} /></Link>
   </div>
 );
 
