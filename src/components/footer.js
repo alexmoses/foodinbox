97d22090ee2eaca92667e742705ba25fc9ff32b1
Alex Moses <alexander_moses@hotmail.com>
2020-09-17 21:39:54 +1000

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,10 +4,8 @@ import { FaFacebook, FaInstagram, FaTwitter, FaPinterest  } from 'react-icons/fa
 
 const Footer = () => (
   <div className="footer">
-   <Link to="https://www.instagram.com/thefoodinbox/"> Instagram</Link>
-   <Link to="https://www.facebook.com/thefoodinbox"> Facebook</Link>
-   <Link to="https://twitter.com/OurFoodInbox"> Twitter </Link>
-   <Link to="https://www.pinterest.com.au/thefoodinbox/"> Pinterest </Link>
+   <FaInstagram />
+   
   </div>
 );
 
