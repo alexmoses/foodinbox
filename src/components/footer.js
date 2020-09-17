25259e4042e495d2037a3653f9999a8505ceda65
Alex Moses <alexander_moses@hotmail.com>
2020-09-17 22:09:07 +1000

    Padding and Icon Size Increase

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,18 @@
 import React from "react";
 import styles from './footer.css';
 import { FaFacebook, FaInstagram, FaTwitter, FaPinterest  } from 'react-icons/fa';
+import { IconContext } from "react-icons/lib";
 
 const Footer = () => (
+
+  <IconContext.Provider value={{size: "2em", className: "footerIcon" }}>
   <div className="footer">
    <a href="https://www.instagram.com/thefoodinbox/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
    <a href="https://www.facebook.com/thefoodinbox" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
    <a href="https://twitter.com/OurFoodInbox" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
    <a href="https://www.pinterest.com.au/thefoodinbox/" target="_blank" rel="noopener noreferrer"><FaPinterest /></a>
   </div>
+  </IconContext.Provider>
 );
 
 export default Footer;
