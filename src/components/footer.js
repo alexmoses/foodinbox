b2eaa9f104e0c74054b7e2f6d7108cf41dd27693
Alex Moses <alexander_moses@hotmail.com>
2020-09-17 21:23:48 +1000

    PLEASE GOD WORK

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,13 @@
 import React from "react";
-import styles from './footer.css'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { facebook, instagram, twitter, pinterest  } from '@fortawesome/free-svg-icons'
-
-library.add(fab, facebook, instagram, twitter, pinterest)
-
+import styles from './footer.css';
+import { FaFacebook, FaInstagram, FaTwitter, FaPinterest  } from 'react-icons/fa';
 
 const Footer = () => (
   <div className="footer">
-   <Link to="https://www.instagram.com/thefoodinbox/"><FontAwesomeIcon icon={["fab", "instagram"]} /></Link>
-   <Link to="https://www.facebook.com/thefoodinbox"><FontAwesomeIcon icon={["fab", "facebook"]} /></Link>
-   <Link to="https://twitter.com/OurFoodInbox"><FontAwesomeIcon icon={["fab", "twitter"]} /></Link>
-   <Link to="https://www.pinterest.com.au/thefoodinbox/"><FontAwesomeIcon icon={["fab", "pinterest"]} /></Link>
+   <Link to="https://www.instagram.com/thefoodinbox/"><FaInstagram/></Link>
+   <Link to="https://www.facebook.com/thefoodinbox"><FaFacebook/></Link>
+   <Link to="https://twitter.com/OurFoodInbox"><FaTwitter/></Link>
+   <Link to="https://www.pinterest.com.au/thefoodinbox/"><FaPinterest/></Link>
   </div>
 );
 
