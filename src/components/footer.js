5118d35df7ae5ac55693689fae4a87db162ae15c
Alex Moses <alexander_moses@hotmail.com>
2020-09-17 21:44:58 +1000

    ALLELUJAGHGHGGIUGG

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,8 +4,10 @@ import { FaFacebook, FaInstagram, FaTwitter, FaPinterest  } from 'react-icons/fa
 
 const Footer = () => (
   <div className="footer">
-   <FaInstagram />
-   
+   <a href="https://www.instagram.com/thefoodinbox/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+   <a href="https://www.facebook.com/thefoodinbox" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+   <a href="https://twitter.com/OurFoodInbox" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+   <a href="https://www.pinterest.com.au/thefoodinbox/" target="_blank" rel="noopener noreferrer"><FaPinterest /></a>
   </div>
 );
 
