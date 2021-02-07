82bb967a38d817f5498833b5d179e165ae13502e
Alex Moses <alexander_moses@hotmail.com>
2021-02-07 15:06:05 +1100

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -15,21 +15,10 @@ class BlogIndex extends React.Component {
       <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
         <Helmet>
-
+        <script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
 <title>{siteTitle}</title>
 
-<script>
-  (function(w, d){
-   var id='embedly-platform', n = 'script';
-   if (!d.getElementById(id)){
-     w.embedly = w.embedly || function() {(w.embedly.q = w.embedly.q || []).push(arguments);};
-     var e = d.createElement(n); e.id = id; e.async=1;
-     e.src = ('https:' === document.location.protocol ? 'https' : 'http') + '://cdn.embedly.com/widgets/platform.js';
-     var s = d.getElementsByTagName(n)[0];
-     s.parentNode.insertBefore(e, s);
-   }
-  })(window, document);
-</script>
+
 </Helmet>
           <div className={styles.hero}>Blog</div>
           <div className="wrapper">
